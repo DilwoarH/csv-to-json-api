97d53693e4960152f2d8915f7cbcecf925f3f92c
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-01-04 17:16:20 +0000

    updates

--- express/server.js
+++ express/server.js
@@ -7,11 +7,17 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  if (!req.query.csv) return res.json({
+    error: "Please pass in CSV url"
+  });
+
+  csv()
+  .fromStream(request.get(req.query.csv))
+  .then((json)=>{
+    res.json(json)
+  });
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
